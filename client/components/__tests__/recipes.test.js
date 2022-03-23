6790b8c86f40a200b80484c4a2d8a46a0d38ed54
Evelyn Tan <evetan.wy@gmail.com>
2022-03-24 10:29:27 +1300

    updated Test script

--- client/components/__tests__/recipes.test.js
+++ client/components/__tests__/recipes.test.js
@@ -1,11 +1,8 @@
-import * as React from "react";
+import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Recipes from "../Recipes";
 
-/**
- * @jest-environment jsdom
- */
 
 test("Test should render Recipes component", () => {
   render(<Recipes />);
@@ -13,7 +10,7 @@ test("Test should render Recipes component", () => {
   expect(recipesElement).toBeInTheDocument();
 });
 
-test("Test if page contain section", () => {
+test("Test if page contain section / labels [Home, Search, Help, Find the food]", () => {
   render(<Recipes />);
   const contactElement = screen.getByTestId("recipe-form");
   expect(contactElement).toHaveTextContent("Home");
